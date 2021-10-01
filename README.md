d4e66b2f619f8103d915fe1078eba114a98c0d95
devkbm <kbm0417@nate.com>
2021-10-01 16:17:55 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ng-zorro-antd-ivy-1h5ibm
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/ng-zorro-antd-ivy-1h5ibm)
---
+++ src/app/my-form-input/my-form-input.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-form-input',
+  templateUrl: './my-form-input.component.html',
+  styleUrls: ['./my-form-input.component.css'],
+})
+export class MyFormInputComponent implements OnInit {
+  value?: string;
+
+  constructor() {}
+
+  ngOnInit() {}
+}
